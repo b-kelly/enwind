da0d9cb577a5115c17edba4f0ede7b1472302e00
Ben Kelly <b-kelly@users.noreply.github.com>
2023-04-15 23:13:21 -0400

    start adding code samples

--- docs/src/lib/Sample.svelte
+++ docs/src/lib/Sample.svelte
@@ -1,10 +1,21 @@
 <script lang="ts">
 	export let title: string = '';
+
+	let childContent: HTMLElement;
 </script>
 
 <div class="flex flex-col gap-2">
 	{#if title}
 		<h3 class="text-xl font-mono">{title}</h3>
 	{/if}
-	<slot />
+	<div class="contents" bind:this={childContent}>
+		<slot />
+	</div>
+	<pre>{childContent?.innerHTML.replace(/>\s*?</g, '>\n<')}</pre>
 </div>
+
+<style>
+	pre {
+		@apply font-mono bg-body-highlight p-2 max-w-full overflow-auto;
+	}
+</style>
