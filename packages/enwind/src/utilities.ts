8cc1adb4fbf2047c7cf4eaae762ba6c82e180e4f
Ben Kelly <b-kelly@users.noreply.github.com>
2023-08-05 17:37:14 -0400

    extension(utilities):  add px, xl, 2xl, 3xl spacing values

--- packages/enwind/src/utilities.ts
+++ packages/enwind/src/utilities.ts
@@ -4,11 +4,15 @@ import defaultTheme from "tailwindcss/defaultTheme";
 /** Enwind specific theme config; values that match the default config are there for strict mode */
 export const utilities: NonNullable<Config["theme"]> = {
     spacing: {
+        px: defaultTheme.spacing["px"],
         0: defaultTheme.spacing[0],
         xs: defaultTheme.spacing[1],
         sm: defaultTheme.spacing[2],
         md: defaultTheme.spacing[4],
         lg: defaultTheme.spacing[6],
+        xl: defaultTheme.spacing[8],
+        "2xl": defaultTheme.spacing[12],
+        "3xl": defaultTheme.spacing[16],
     },
     borderRadius: {
         DEFAULT: defaultTheme.borderRadius.DEFAULT,
