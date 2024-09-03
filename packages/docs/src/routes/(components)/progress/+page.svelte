0190482e897c15647f6cc2e2276eafcb6418e9d9
Ben Kelly <b-kelly@users.noreply.github.com>
2024-09-03 14:13:13 -0400

    enwind: add progress bar component

--- packages/docs/src/routes/(components)/progress/+page.svelte
+++ packages/docs/src/routes/(components)/progress/+page.svelte
@@ -3,15 +3,49 @@
 	import Section from '$lib/Section.svelte';
 </script>
 
-<Section title="Progress">
+<Section title="Progress bar">
+	<Sample
+		class="flex flex-col gap-md"
+		content={`<div
+	class="progress"
+	role="progressbar"
+	aria-label="current progress"
+	aria-valuemin="0"
+	aria-valuemax="100"
+	aria-valuenow="50"
+	style="--value: 50">
+</div>
+
+<!-- Bar color -->
+<div class="progress text-success" style="--value: 75"></div>`}
+	>
+		<div class="progress" style:--value={0}></div>
+		<div class="progress" style:--value={50}></div>
+		<div class="progress" style:--value={100}></div>
+		<div class="progress text-success" style:--value={75}></div>
+	</Sample>
+</Section>
+
+<Section title="Progress circle">
 	<Sample
 		class="flex gap-md"
-		content={`<div class="progress-circle" style="--value: 0">0%</div>
-<div class="progress-circle" style="--value: 50">50%</div>
-<div class="progress-circle" style="--value: 100">100%</div>`}
+		content={`<div class="progress-circle"
+	role="progressbar"
+	aria-label="current progress"
+	aria-valuemin="0"
+	aria-valuemax="100"
+	aria-valuenow="50"
+	style="--value: 50">
+	50%
+</div>
+
+<!-- Bar color -->
+<div class="progress-circle text-success" style="--value: 75">75%</div>`}
 	>
 		<div class="progress-circle" style="--value: 0">0%</div>
 		<div class="progress-circle" style="--value: 50">50%</div>
 		<div class="progress-circle" style="--value: 100">100%</div>
+
+		<div class="progress-circle text-success" style="--value: 75">75%</div>
 	</Sample>
 </Section>
--- packages/enwind/src/components/progress.ts
+++ packages/enwind/src/components/progress.ts
@@ -1,6 +1,22 @@
 import { CssInJs } from "../types";
 
 const progress: CssInJs = (theme) => ({
+    ".progress": {
+        "--value": "0",
+        backgroundColor: theme("colors.body.highlight"),
+        borderRadius: theme("borderRadius.DEFAULT"),
+        color: theme("colors.primary.DEFAULT"),
+        height: theme("height.sm"),
+        overflow: "hidden",
+        width: "100%",
+        "&:before": {
+            content: "''",
+            backgroundColor: "currentColor",
+            display: "block",
+            height: theme("height.sm"),
+            width: "calc(var(--value, 0) * 1%)",
+        },
+    },
     ".progress-circle": {
         "--value": "0",
         "--width": "8rem", //TODO theme("width.32"),
