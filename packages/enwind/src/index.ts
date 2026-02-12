34c3c6c0d032f43efa56366a3410b9bdcc9855e2
Ben Kelly <b-kelly@users.noreply.github.com>
2026-02-12 11:39:53 -0500

    migrate colors and base

--- packages/enwind/src/index.ts
+++ packages/enwind/src/index.ts
@@ -1,7 +1,5 @@
 import plugin, { Config } from "tailwindcss/plugin";
-import base from "./base.js";
 import components from "./components/index.js";
-import colors from "./colors.js";
 import { utilities } from "./utilities.js";
 import { CssInJs } from "./types.js";
 
@@ -13,61 +11,14 @@ export interface EnwindOptions {
 
 export default plugin.withOptions<EnwindOptions>(
     () =>
-        function ({ addBase, theme, addComponents }) {
-            addBase(base(theme));
+        function ({ theme, addComponents }) {
             addComponents(components(theme) as Record<string, CssInJs>);
         },
     (opts) => {
         const config: Partial<Config> = {
             theme: {
-                enwind: {
-                    colors,
-                },
                 extend: {
                     ...utilities,
-                    colors: {
-                        body: {
-                            DEFAULT: "var(--_bw-color-body)",
-                            content: {
-                                DEFAULT: "var(--_bw-color-body-content)",
-                                light: "var(--_bw-color-body-content-light)",
-                                dark: "var(--_bw-color-body-content-dark)",
-                            },
-                            highlight: "var(--_bw-color-body-highlight)",
-                            overlay: "var(--_bw-color-body-overlay)",
-                        },
-                        primary: {
-                            DEFAULT: "var(--_bw-color-primary)",
-                            content: "var(--_bw-color-primary-content)",
-                            focus: "var(--_bw-color-primary-focus)",
-                        },
-                        success: {
-                            DEFAULT: "var(--_bw-color-success)",
-                            focus: "var(--_bw-color-success-focus)",
-                        },
-                        warn: {
-                            DEFAULT: "var(--_bw-color-warn)",
-                            focus: "var(--_bw-color-warn-focus)",
-                        },
-                        danger: {
-                            DEFAULT: "var(--_bw-color-danger)",
-                            focus: "var(--_bw-color-danger-focus)",
-                        },
-                        code: {
-                            DEFAULT: "var(--_bw-color-code)",
-                            addition: "var(--_bw-color-code-addition)",
-                            attr: "var(--_bw-color-code-attr)",
-                            builtin: "var(--_bw-color-code-builtin)",
-                            comment: "var(--_bw-color-code-comment)",
-                            deletion: "var(--_bw-color-code-deletion)",
-                            literal: "var(--_bw-color-code-literal)",
-                            meta: "var(--_bw-color-code-meta)",
-                            string: "var(--_bw-color-code-string)",
-                            tag: "var(--_bw-color-code-tag)",
-                            title: "var(--_bw-color-code-title)",
-                            variable: "var(--_bw-color-code-variable)",
-                        },
-                    },
                 },
             },
         };
