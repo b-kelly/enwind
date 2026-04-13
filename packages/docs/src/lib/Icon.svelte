368bc70b7f1010464b0ad944b7c4940cf48b0ef0
Ben Kelly <b-kelly@users.noreply.github.com>
2026-04-13 19:40:03 -0400

    reenable commented out svelte components in docs

--- packages/docs/src/lib/Icon.svelte
+++ packages/docs/src/lib/Icon.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	// import { Icon } from '@enwindui/svelte';
+	import { Icon } from '@enwindui/svelte';
 	import adjustments from 'heroicons/20/solid/adjustments-horizontal.svg?raw';
 	import bars3 from 'heroicons/20/solid/bars-3.svg?raw';
 	import play from 'heroicons/20/solid/play.svg?raw';
@@ -23,4 +23,4 @@
 	let { icon }: Props = $props();
 </script>
 
-<!-- <Icon icon={icons[icon]} /> -->
+<Icon icon={icons[icon]} />
